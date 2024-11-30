13a51ea70be3924114a866dfdcb5d4f10fc1ad05
shahadot99999 <shahadot99999@gmail.com>
2024-11-30 07:37:09 +0600

    change home screen to play ground

--- script/alpha-class.js
+++ script/alpha-class.js
@@ -1,3 +1,19 @@
+// function play(){
+//     //console.log('play start now')
+
+//     //show home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     //console.log(homeSection.classList)
+
+//     //show play ground
+//     const playgroundSection = document.getElementById('play-ground');
+//     //console.log(playgroundSection.classList)
+//     playgroundSection.classList.remove('hidden')
+// }
+
 function play(){
-    console.log('play start now')
+    hideElementById('home-screen');
+
+    showElementById('play-ground');
 }
