6b3a766a9b390fdba2f2890a7e69b92fc2a55571
shahadot99999 <shahadot99999@gmail.com>
2024-12-02 16:31:32 +0600

    high light selected alphabet

--- script/utility.js
+++ script/utility.js
@@ -8,20 +8,38 @@ function showElementById(elementId){
     element.classList.remove('hidden');
 }
 
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
 function getARandomAlphabet(){
-    //get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetString.split ('');
-    //console.log(alphabets)
+    const alphabets = alphabetString.split('');
 
 
-    // get a random index between 0-25
     const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
-    //console.log(index);
+
 
     const alphabet = alphabets[index];
-    //console.log(index, alphabet);
     return alphabet;
-
 }
+// function getARandomAlphabet(){
+//     //get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split ('');
+//     //console.log(alphabets)
+
+
+//     // get a random index between 0-25
+//     const randomNumber = Math.random()*25;
+//     const index = Math.round(randomNumber);
+//     //console.log(index);
+
+//     const alphabet = alphabets[index];
+//     //console.log(index, alphabet);
+//     return alphabet;
+
+// }
