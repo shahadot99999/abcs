1173fdad6585705ed14a004e0a33b884564014e5
shahadot99999 <shahadot99999@gmail.com>
2024-12-03 18:35:16 +0600

    Updated score and set score

--- script/alpha-class.js
+++ script/alpha-class.js
@@ -38,12 +38,46 @@ function handleKeyboardKeyUPEvent(event){
     if(playerPress === expectedAlphabet)
     {
         console.log('you got point');
-        console.log('you have presssed correctly', playerPress);
+        //console.log('you have presssed correctly', playerPress);
+
+       //update a score:
+       //1.get the current score
+
+
+       const currentScoreElement = document.getElementById('current-score');
+       const currentScoreText = currentScoreElement.innerText;
+       //console.log(currentScoreText);
+       const currentScore = parseInt(currentScoreText);
+       console.log(currentScore);
+       //2. Increase the new score by 1
+
+       const newScore = currentScore + 1;
+
+       //3. show the up date score
+       currentScoreElement.innerText = newScore;
+
+
+        
+
+        //start a new round
         removeBackgroundColorById(playerPress);
         continueGame();
     }
     else{
         console.log('you can not get point');
+
+        //step 1 : get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+
+        //step 2 : reduce the life count
+        const newLife = currentLife - 1 ;
+
+
+        //step 3: display the updated life count
+        currentLifeElement.innerText = newLife;
     }
 
 }
